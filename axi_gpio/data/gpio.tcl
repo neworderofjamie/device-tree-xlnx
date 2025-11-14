94ce8ee16bb5256d48f6eb0fcfbecd66aec24516
neworderofjamie <neworderofjamie@gmail.com>
2025-11-14 17:52:50 +0000

    and again

--- axi_gpio/data/gpio.tcl
+++ axi_gpio/data/gpio.tcl
@@ -27,7 +27,8 @@ proc generate {drv_handle} {
 	if {$node == 0} {
 		return
 	}
-	set compatible ["generic-uio"]
+	set compatible [get_comp_str $drv_handle]
+	set compatible [append compatible " " "generic-uio"]
 	set_drv_prop $drv_handle compatible "$compatible" stringlist
 	set intr_present [get_property CONFIG.C_INTERRUPT_PRESENT [get_cells -hier $drv_handle]]
 	if {[string match $intr_present "1"]} {
